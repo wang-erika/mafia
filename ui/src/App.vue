69e31dd6e97ffebd4568cd77430575acaeab90c0
Erika <erikawang2020@gmail.com>
2024-04-14 17:56:02 -0400

    GraphQL.vue queries successfully

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,3 +1,21 @@
 <template>
   <router-view />
 </template>
+
+<script setup lang="ts">
+import { provide } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+
+const httpLink = createHttpLink({
+  // see https://studio.apollographql.com/public/SpaceX-pxxbxen/variant/current/home
+  uri: 'http://localhost:8131/graphql',
+})
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+provide(DefaultApolloClient, apolloClient)
+</script>
--- ui/src/views/GraphQL.vue
+++ ui/src/views/GraphQL.vue
@@ -1,12 +1,57 @@
 <template>
-    <div>
-      <h1>GraphQL Component</h1>
+    <div class="mx-3 my-3">
+        <h1>GameState</h1>
+        <div v-if="loading">Loading...</div>
+        <div v-if="error">{{ error.message }}</div>
+        <p> Phase and Round: {{ result.gameState.phase }} {{ result.gameState.round }}</p>
+
+        <table v-if="result && result.gameState && result.gameState.players && result.gameState.players.length">
+            <thead>
+                <tr>
+                    <th>Player Name</th>
+                    <th>Role</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="player in result.gameState.players" :key="player.id">
+                    <td>{{ player.name }}</td>
+                    <td>{{ player.role }}</td>
+                    <td>{{ player.status }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-else>
+            No players data available.
+        </div>
     </div>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue';
-  export default defineComponent({
-    name: 'GraphQL'
-  });
-  </script>
+</template>
+
+
+<script setup lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+import { computed } from 'vue';
+
+const { result, loading, error } = useQuery(gql`
+    query ExampleQuery {
+        gameState {
+            _id
+            phase
+            players {
+                name
+                role
+                status
+                id
+                killVote
+                votes
+            }
+            round
+        }
+    }
+
+`);
+
+// Log to check what's inside result
+console.log(result);
+</script>
