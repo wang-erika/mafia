b34845f1f2b5188f22b6e40f335840b1436eb5ab
Jaewon Jung <jaewon.j.1@gmail.com>
2024-04-19 18:58:48 -0400

    removed merge conflict marker

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,13 +4,10 @@ import App from './App.vue';
 import Chat from './views/Chat.vue';
 import LoginPage from './views/LoginPage.vue'
 import Vote from './views/Vote.vue'
-<<<<<<< HEAD
 import GameLobby from './views/GameLobby.vue'
-=======
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
->>>>>>> 7ae65b485cb3d70f7da75e09578bc97c6e075a8d
 
 const routes = [
   {
