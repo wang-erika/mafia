03e5ef731b8d995386756d27bfcd007b9700df2e
Jaewon Jung <jaewon.j.1@gmail.com>
2024-04-21 21:39:10 -0400

    fixed test case

--- tests/mafia.spec.js
+++ tests/mafia.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('bypass login using disable-security strategy', async ({ page }) => {
-    await page.goto('http://localhostasdfadfas:31000/api/auth?key=disable-security&nickname=test&name=test');
+    await page.goto('http://localhost:31000/api/auth?key=disable-security&nickname=test&name=test');
   
     const response = await page.request.get('http://localhost:31000/api/check');
     const responseBody = await response.json();
